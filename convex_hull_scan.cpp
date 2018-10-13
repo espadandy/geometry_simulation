47ecde2d2dea900290365b1c57f7786efb46ea39
espadandy <espadandy@gmail.com>
2018-10-13 01:31:28 -0400

    add more stuff to convex hull scan

--- convex_hull_scan.cpp
+++ convex_hull_scan.cpp
@@ -9,18 +9,30 @@
 
 using namespace std;
 
-
 // left bottom are precedence
 class pointComparison {
 public:
     bool operator() (const array<int,2>& lhs, const array<int,2>& rhs) {
-        if ((lhs[0] < rhs[0]) || (lhs[0] == rhs[0] && lhs[1] < rhs[1]))
-            return true;
-        else
-            return false;
+        return (lhs[0] < rhs[0]) || (lhs[0] == rhs[0] && lhs[1] < rhs[1]);
     }
 }ope;
 
-void convex_hull_scan(std::vector<std::array<int,2>>& points) {
+bool isLeftTurn(array<int,2> p0, array<int,2> p1, array<int,2> p2) {
+    return true;
+}
+
+vector<array<int,2>> convex_hull_scan(std::vector<std::array<int,2>>& points) {
     std::sort(points.begin(), points.end(), ope);
+    if (points.size() < 3) {
+        return points;
+    }
+
+    vector<array<int,2>> upper = {points[0], points[1]};
+
+    for (size_t i = 2; i < points.size(); i++) {
+        upper.push_back(points[i]);
+        while (isLeftTurn(*(upper.end() - 3), *(upper.end()-2), *(upper.end()-1))) {
+            upper.erase(upper.end() - 1);
+        }
+    }
 }
--- convex_hull_scan.h
+++ convex_hull_scan.h
@@ -2,12 +2,14 @@
 // Created by Yicong Xu on 8/19/18.
 //
 
-#include <vector>
-#include <array>
-
 #ifndef SRC_CONVEX_HULL_SCAN_H
 #define SRC_CONVEX_HULL_SCAN_H
 
-void convex_hull_scan(std::vector<std::array<int,2>>& points);
+#include <vector>
+#include <array>
+
+std::vector<std::array<int,2>> convex_hull_scan(std::vector<std::array<int,2>>& points);
+
+bool isLeftTurn(std::array<int,2>, std::array<int,2>, std::array<int,2>);
 
 #endif //SRC_CONVEX_HULL_SCAN_H
